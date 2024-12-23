a10d46c91558eb8aeb59aa736a772f830777a766
Daniele <danielesoudapaz11@gmail.com>
2024-12-23 10:42:28 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MeuAplicativo extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const PrimeiraPagina(titulo: 'Primeiro Programa'),
+      home: const PrimeiraPagina(titulo: 'Daniele'),
     );
   }
 }
